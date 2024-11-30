cf6cd007b4d8934a98630f3224bf38497198a9d3
João Vitor Simão Pinheiro <joaovitor26.2002@gmail.com>
2024-11-29 23:11:20 -0300

    Recuperando parametros

--- src/main/java/com/example/prjgestao/controller/exemploController.java
+++ src/main/java/com/example/prjgestao/controller/exemploController.java
@@ -1,10 +1,15 @@
 package com.example.prjgestao.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.prjgestao.classes.ListaProdutos;
+import com.example.prjgestao.classes.PlacaDeVideo;
 
 
 
@@ -18,8 +23,16 @@ public class exemploController {
         this.listaProdutos = listaProdutos;
     }
 
-    @GetMapping("/dados")
-    public ListaProdutos exemplo() {
+    @PostMapping("/dados")
+    public ListaProdutos exemplo(@RequestBody Map<String, String> argumentos) {
+        String placaDeVideo = argumentos.get("placaDeVideo"); 
+        String processador = argumentos.get("processador");
+
+        List<PlacaDeVideo> listaPlacasDeVideo = listaProdutos.getListaPlacaDeVideos();
+        for(PlacaDeVideo listPlacaDeVideo : listaPlacasDeVideo) {
+            
+        }
+
         return listaProdutos;
     }
     
